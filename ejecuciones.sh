58ce2ffa4d0d4932af7eb95cb918c7dd66ab7f6b
Antonio David Villegas <advy99@gmail.com>
2020-04-15 22:38:28 +0200

    hacemos la ejecucion en todos los procesadores a la vez

--- ejecuciones.sh
+++ ejecuciones.sh
@@ -7,16 +7,18 @@ for semilla in ${semillas[*]}
 do
 	echo "Ejecutando los distintos datasets con la semilla $semilla"
 
-	./bin/practica1 datos/iris_set.dat datos/iris_set_const_10.const 3 $semilla
-	./bin/practica1 datos/iris_set.dat datos/iris_set_const_20.const 3 $semilla
+	./bin/practica1 datos/iris_set.dat datos/iris_set_const_10.const 3 $semilla &
+	./bin/practica1 datos/iris_set.dat datos/iris_set_const_20.const 3 $semilla &
 
-	./bin/practica1 datos/rand_set.dat datos/rand_set_const_10.const 3 $semilla
-	./bin/practica1 datos/rand_set.dat datos/rand_set_const_20.const 3 $semilla
+	./bin/practica1 datos/rand_set.dat datos/rand_set_const_10.const 3 $semilla &
+	./bin/practica1 datos/rand_set.dat datos/rand_set_const_20.const 3 $semilla &
 
-	./bin/practica1 datos/newthyroid_set.dat datos/newthyroid_set_const_10.const 3 $semilla
-	./bin/practica1 datos/newthyroid_set.dat datos/newthyroid_set_const_20.const 3 $semilla
+	./bin/practica1 datos/newthyroid_set.dat datos/newthyroid_set_const_10.const 3 $semilla &
+	./bin/practica1 datos/newthyroid_set.dat datos/newthyroid_set_const_20.const 3 $semilla &
 
-	./bin/practica1 datos/ecoli_set.dat datos/ecoli_set_const_10.const 8 $semilla
-	./bin/practica1 datos/ecoli_set.dat datos/ecoli_set_const_20.const 8 $semilla
+	./bin/practica1 datos/ecoli_set.dat datos/ecoli_set_const_10.const 8 $semilla &
+	./bin/practica1 datos/ecoli_set.dat datos/ecoli_set_const_20.const 8 $semilla &
+
+	wait
 
 done
