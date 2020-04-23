e3b73c232cff9802c2d262391e80cbf10fa98b1e
Antonio David Villegas <advy99@gmail.com>
2020-04-23 13:25:00 +0200

    reordenamos para si hacemos ejecuciones de 4 en lugar de 8 procesadores

--- ejecuciones.sh
+++ ejecuciones.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-
+# semillas
 semillas=(123452244 9398429 12321 213566 3939021)
 
 for semilla in ${semillas[*]}
 do
 	echo "Ejecutando los distintos datasets con la semilla $semilla"
 
+	./bin/practica1 datos/ecoli_set.dat datos/ecoli_set_const_10.const 8 $semilla &
+	./bin/practica1 datos/ecoli_set.dat datos/ecoli_set_const_20.const 8 $semilla &
+
+	./bin/practica1 datos/newthyroid_set.dat datos/newthyroid_set_const_10.const 3 $semilla &
+	./bin/practica1 datos/newthyroid_set.dat datos/newthyroid_set_const_20.const 3 $semilla &
+
 	./bin/practica1 datos/iris_set.dat datos/iris_set_const_10.const 3 $semilla &
 	./bin/practica1 datos/iris_set.dat datos/iris_set_const_20.const 3 $semilla &
 
 	./bin/practica1 datos/rand_set.dat datos/rand_set_const_10.const 3 $semilla &
 	./bin/practica1 datos/rand_set.dat datos/rand_set_const_20.const 3 $semilla &
-
-	./bin/practica1 datos/newthyroid_set.dat datos/newthyroid_set_const_10.const 3 $semilla &
-	./bin/practica1 datos/newthyroid_set.dat datos/newthyroid_set_const_20.const 3 $semilla &
-
-	./bin/practica1 datos/ecoli_set.dat datos/ecoli_set_const_10.const 8 $semilla &
-	./bin/practica1 datos/ecoli_set.dat datos/ecoli_set_const_20.const 8 $semilla &
 
 	wait
 
